2ed39031e188b4b7edd834bbbd4988aae3244a4e
C.M.T <c.min.tseng@gmail.com>
2021-06-07 13:02:34 +0800

    fix koin  androidLogger crash

--- app/src/main/java/com/sion/itunes/App.kt
+++ app/src/main/java/com/sion/itunes/App.kt
@@ -9,6 +9,7 @@ import com.sion.itunes.widget.log.DebugLogTree
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 import timber.log.Timber
 
 class App: Application() {
@@ -31,7 +32,7 @@ class App: Application() {
         )
 
         startKoin {
-            androidLogger()
+            androidLogger(Level.ERROR)
             androidContext(this@App)
             modules(module)
         }
